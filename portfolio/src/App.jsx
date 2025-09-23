dd2a84c57878d3bdf15cb5075989723a6ad37c03
Hiumi112688 <ar112688@fhss.sjp.ac.lk>
2025-09-23 12:20:16 +0530

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,25 +1,33 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import './App.css';
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
-  
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark'); 
+    } else {
+      document.documentElement.classList.remove('dark'); 
+    }
+  }, [darkMode]);
 
   return (
-    <div >
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-      
+    <div className="bg-gray-100 dark:bg-gray-900 min-h-screen transition-colors">
+      <Navbar toggleDarkMode={() => setDarkMode(!darkMode)} />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
