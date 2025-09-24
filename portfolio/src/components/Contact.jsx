fa8f2a50393a7ae9236f62dbb6abafeb0f0973db
Hiumi112688 <ar112688@fhss.sjp.ac.lk>
2025-09-24 19:25:17 +0530

    Update Contact.jsx

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -5,7 +5,7 @@ import { FaEnvelope, FaPhone, FaMapMarkedAlt } from "react-icons/fa"
 
 const Contact = () => {
   return (
-    <div className="bg-black text-white py-20" id="contact">
+    <div className="bg-black text-white py-20 bg-black dark:bg-gray-900 text-white " id="contact">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">Contact Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
