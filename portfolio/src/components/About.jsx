6505e0b59db9cd0c06881f4c37dfff2ab1002a68
Hiumi112688 <ar112688@fhss.sjp.ac.lk>
2025-09-24 16:20:28 +0530

    Update About.jsx

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 const About = () => {
     return (
+        
         <div className='bg-black text-white py-20' id='about'>
             <div className='container mx-auto px-8 md:px-16 lg:px-24'>
                 <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
@@ -49,6 +50,7 @@ const About = () => {
                 </div>
             </div>
         </div>
+        
     )
 }
 
