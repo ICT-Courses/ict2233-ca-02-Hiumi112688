4ee3e0065e4c9521398165e09da7453c79250ff6
Hiumi112688 <ar112688@fhss.sjp.ac.lk>
2025-09-24 16:20:37 +0530

    Update Hero.jsx

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import React  from 'react'
 // import HeroImage from '../assets/profile.jpg'
 
+
 const Hero =() =>{
     return (
+        
         <div className='bg-black text-white text-center py-16'>
             <img src='\src\assets\profile.jpg'
             className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105' />
@@ -19,6 +21,7 @@ const Hero =() =>{
                 <button className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full '>Resume</button>
             </div>
         </div>
+        
     )
 }
 
